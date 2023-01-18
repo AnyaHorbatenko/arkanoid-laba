29410029bcb26a545429c951dd8c454e401ed714
shito.bulo@gmail.com <shito.bulo@gmail.com>
2023-01-18 14:19:09 +0200

    Added paddle collision

--- main.py
+++ main.py
@@ -3,6 +3,28 @@ import pgzrun
 WIDTH = 800
 HEIGHT = 600
 
+class Paddle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.rect = pg.Rect((self.x, self. y), (self.width, self.height))
+
+    def draw(self):
+        screen.draw.filled_rect(Rect((self.x, self.y), (self.width, self.height)), (255, 255, 255))
+
+    def update(self):
+        self.x = pg.mouse.get_pos()[0]
+        if self.x < -100:
+            self.x = 0
+        if self.x > WIDTH-50:
+            self.x = WIDTH-100
+        
+    def check_collision(self, ball):
+        if ball.x > self.x and ball.x < self.x + self.width and ball.y > self.y and ball.y < self.y + self.height:
+            ball.VelocityY = -ball.VelocityY
+
 class Ball:
     def __init__(self, x, y, radius):
         self.x = x
@@ -23,13 +45,20 @@ class Ball:
         if self.y > HEIGHT or self.y < 0:
             self.VelocityY = -self.VelocityY
 
+ball = Ball(400, 300, 10)
+paddle = Paddle(120, 550, 100, 100)
 
 def draw():
-    pass
+    screen.clear()
+    screen.fill((123, 221, 65))
+    screen.draw.text("Score: " + str(score), (500, 500), color=(0, 0, 0), background="white")
+    ball.draw()
+    paddle.draw()
    
 
 def update(dt):
-    pass
+    ball.update()
+    paddle.update()
 
 
 pgzrun.go()
